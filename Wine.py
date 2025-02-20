65cf2bc026af6cdad59bb117d3d0414b974f23c7
vbacho <victorbachour@gmail.com>
2025-02-20 15:41:23 -0800

    inital commit

---
+++ Wine.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+class Model(nn.Module):
+    def __init__(self, in_features=11, h1=8, h2=9, out_features=1):
+        super().__init__()
+
+
+
+if __name__ == "__main__":
+    Model()
