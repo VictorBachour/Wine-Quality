d81b38e7a1b3a95796153a78edbb2b85db501e7b
vbacho <victorbachour@gmail.com>
2025-03-01 13:56:22 -0800

    a little bit of work on the model

--- Wine.py
+++ Wine.py
@@ -1,11 +1,32 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import pandas as pd
 class Model(nn.Module):
     def __init__(self, in_features=11, h1=8, h2=9, out_features=1):
         super().__init__()
+        self.fc1 = nn.Linear(in_features, h1)
+        self.fc2 = nn.Linear(h1, h2)
+        self.out = nn.Linear(h2, out_features)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.out(x)
+
+        return x
+
 
 
 
 if __name__ == "__main__":
-    Model()
+    model = Model()
+
+    url = "https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
+    df = pd.read_csv(url, sep=';')
+    X = df.drop(columns=['quality'])
+    y = df['quality']
+
+    X_tensor = torch.tensor(X.values, dtype=torch.float32)
+    y_tensor = torch.tensor(y.values, dtype=torch.float32).view(-1,1)
+
