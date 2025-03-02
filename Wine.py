add0f5f3be4e8923b052b7970dd668e68d8c5351
vbacho <victorbachour@gmail.com>
2025-03-02 13:32:16 -0800

    working on epochs

--- Wine.py
+++ Wine.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 class Model(nn.Module):
     def __init__(self, in_features=11, h1=8, h2=9, out_features=1):
         super().__init__()
@@ -27,6 +29,17 @@ if __name__ == "__main__":
     X = df.drop(columns=['quality'])
     y = df['quality']
 
-    X_tensor = torch.tensor(X.values, dtype=torch.float32)
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+
+    X_tensor = torch.tensor(X_scaled, dtype=torch.float32)
     y_tensor = torch.tensor(y.values, dtype=torch.float32).view(-1,1)
 
+    X_train, X_test, y_train, y_test = train_test_split(X_tensor, y_tensor, test_size=.2, random_state=42)
+
+
+    loss = nn.MSELoss()
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=.01)
+
+    best_loss = float()
